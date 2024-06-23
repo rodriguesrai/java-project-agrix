93df354f61c9a730122543145c6ddac7bff7bd26
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-06-23 10:12:04 -0300

    .

--- src/main/java/com/betrybe/agrix/AgrixApplication.java
+++ src/main/java/com/betrybe/agrix/AgrixApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Application main class.
+ * Application Agrix main class .
  */
 @SpringBootApplication
 public class AgrixApplication {
